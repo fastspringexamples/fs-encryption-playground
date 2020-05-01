616f9a3831c928a6817cd88ae4837c173a4d2e7a
Javier Trujillo <jtrujillo@fastspring.com>
2020-05-01 12:43:42 +0200

    remove unneeded endpoints

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const express = require('express');
 const util = require('util');
-const path = require('path');
-
-const fs = require('fs');
-
-const privateKeyPath = path.resolve(__dirname, 'keys/privatekey.pem');
-const publicCertPath = path.resolve(__dirname, 'keys/publiccert.crt');
-
 
 const encryptor = require('./utils/encryptor.js');
 
@@ -17,21 +10,11 @@ app.use(express.static('public'));
 
 const port = 9000;
 
-
 app.post('/encrypt', (req, res) => {
-    console.log(util.inspect(req.body, false, null, true));
     const payload = encryptor.encrypt(req.body, req.body.customKey);
-    console.log(payload);
     res.json(payload);
 });
 
-
-app.get('/keys', (req, res) => {
-    const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
-    const publicCrt = fs.readFileSync(publicCertPath, 'utf8');
-
-    res.json({ privateKey, publicCrt });
-});
 
 app.get('/keys/new', async (req, res) => {
     const { privateKey, publicCrt } = await encryptor.createNewKeys();
